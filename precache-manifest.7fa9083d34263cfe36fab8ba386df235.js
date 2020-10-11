064f8e86918b1aba183342d2a72b6726fabf3929
Alex Kurniawan <64827057+alexkurniawan-ng@users.noreply.github.com>
2020-10-11 14:08:48 +0700

    Updates

---
+++ precache-manifest.7fa9083d34263cfe36fab8ba386df235.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b6ca654c68e61ae8455e6bd7dc6961",
+    "url": "/alexkurniawan-ng/toko-olahraga/index.html"
+  },
+  {
+    "revision": "17493c67df7bbbb8467e",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "930f4486badb76ced4d0",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/css/main.6cf23a37.chunk.css"
+  },
+  {
+    "revision": "17493c67df7bbbb8467e",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/js/2.f132e911.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/js/2.f132e911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930f4486badb76ced4d0",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/js/main.bfd793c2.chunk.js"
+  },
+  {
+    "revision": "210ea1f352465a38c958",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/js/runtime-main.e2e2a558.js"
+  },
+  {
+    "revision": "8369382dfce8a0577190694cb8a76ff8",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/media/logoA.8369382d.png"
+  },
+  {
+    "revision": "1406fabfc09ed1baa3c9885db00a22aa",
+    "url": "/alexkurniawan-ng/toko-olahraga/static/media/logoBwhite.1406fabf.png"
+  }
+]);
